6692d39a7e262eebde3dd150641c145b75f9ac04
Khamou <108040130+Ihiaoui@users.noreply.github.com>
2025-10-07 14:02:32 +0100

    Delete backend/backend/models directory

--- backend/backend/models/Image.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ImageSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  title: { type: String },
-  uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Image', ImageSchema);
